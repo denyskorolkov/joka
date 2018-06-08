c70ce683eebc3e920defe22e9113d54f534edcf4
Denys Korolkov <denys@korolkov.pro>
2018-06-08 18:00:13 -0400

    + (core, test) ADD `getRelevantId()`;

--- src/core/handler.ts
+++ src/core/handler.ts
@@ -15,7 +15,7 @@ export const handlerSettings = new WeakMap<object, HandlerSetting>();
 let countId: number = 0;
 
 /**
- * Set if property id doesn't exist and get it
+ * Set id if property that doesn't exist and get it
  *
  * @export
  * @this {object} Instance
@@ -24,7 +24,7 @@ let countId: number = 0;
  * @return {string} Id associated with `this` instance
  */
 export function setId(this: object, name: string = ``, isPrefix: boolean = false) {
-	// exe("log", "handler", this, name);
+	// TODO: exe("log", "handler", this, name);
 	let handlerSetting = handlerSettings.get(this);
 
 	if (handlerSetting) {
@@ -42,7 +42,115 @@ export function setId(this: object, name: string = ``, isPrefix: boolean = false
 	return name;
 }
 
+const BRACKETS = /[()]/g;
+const BRACKETS_TRIM = /\(([^)]*)\)/gi;
+
+/**
+ * Get relevant id
+ *
+ * @export
+ * @param {string | string[]} id Either ids array or single id
+ * @returns {string} The most relevant id
+ */
+export function getRelevantId(id: string): string;
+export function getRelevantId(id: string[]): string;
+export function getRelevantId(id: any): string {
+	if (Array.isArray(id)) {
+		if (id[0].search(BRACKETS) != -1) {
+			let rule: string = id.shift();
+			let action = rule.replace(BRACKETS, ``);
+
+			if (has(action)) {
+				id = action;
+			} else {
+				for (let pattern of id) {
+					if (
+						(action = rule
+							.replace(BRACKETS_TRIM, pattern)
+							.replace(/\s+/g, ` `)
+							.trim())
+					) {
+						if (has(action)) {
+							id = action;
+							break;
+						}
+					}
+				}
+			}
+		} else {
+			for (let action of id) {
+				if (has(action)) {
+					id = action;
+					break;
+				}
+			}
+		}
+	} else {
+		id = has(id) ? id : undefined;
+	}
+	return id;
+}
+
+/**
+ * Check existing of current id
+ *
+ * @export
+ * @param {string} id
+ * @returns {boolean}
+ */
+export function has(id: string): boolean {
+	return typeof handlers[id] != 'undefined';
+}
+
+// export function setInstance(instance: object, options: object): void {
+// 	getArray(instance).forEach((item) => {});
+// }
+
+export function get(id: string, path: string): any {}
+
+export function set(id: string, data: any): void;
+export function set(data: object[]): void;
+export function set(data: Function): void;
+export function set(data: object): void;
+export function set(this: any, a: any, b?: any): void {
+	let type = typeof a;
+
+	if (type == 'string') {
+		handlers[a] = b;
+
+		if (!this) {
+			return;
+		}
+
+		let id = this.id || this;
+
+		if (id) {
+			let keyIds = `${id} ids`;
+			if (has(keyIds)) {
+				// push(keyIds, a);
+			} else {
+				set(keyIds, [a]);
+			}
+		}
+	} else if (type == 'object') {
+		for (let key in a) {
+			if (typeof key == 'number') {
+				set.call(this, a[key]);
+			} else {
+				set.call(this, key, a[key]);
+			}
+		}
+	} else if (type == 'function') {
+		set.call(this, a.name, a);
+	} else {
+		throw new Error('Arguments are not correct');
+	}
+}
+
 export default {
+	has,
+	set,
 	setId,
-	handlers
+	handlers,
+	getRelevantId
 };
--- tests/unit/core/handler.ts
+++ tests/unit/core/handler.ts
@@ -1,15 +1,39 @@
-import { setId } from '../../../src/core/handler';
+import { setId, getRelevantId, handlers } from '../../../src/core/handler';
 
 const { registerSuite } = intern.getInterface('object');
 const { assert } = intern.getPlugin('chai');
 
-registerSuite(`jOka => handlers`, {
+registerSuite(`joka => handlers`, {
 	setId() {
 		let obj = {};
+
 		assert.equal(setId.call({}), `#0`);
 		assert.equal(setId.call(obj), `#1`);
 		assert.equal(setId.call({}, `name`), `name`);
 		assert.equal(setId.call({}, `prefix`, true), `prefix#2`);
 		assert.equal(setId.call(obj), `#1`);
+	},
+
+	getRelevantId() {
+		handlers[`test get relevant id`] = true;
+		handlers[`test get relevant id #1`] = true;
+		handlers[`test get relevant id #2`] = true;
+		handlers[`test get relevant id #2 context`] = true;
+
+		assert.equal(getRelevantId(`test get relevant id #1`), `test get relevant id #1`);
+		assert.equal(getRelevantId(`test get relevant id #3`), undefined);
+
+		assert.equal(getRelevantId([`test get relevant id #1`, `test get relevant id #2`]), `test get relevant id #1`);
+
+		assert.equal(
+			getRelevantId([`test get relevant id unknown`, `test get relevant id #2`]),
+			`test get relevant id #2`
+		);
+
+		assert.equal(getRelevantId([`test get relevant id (#2 context)`, `#2`, ``]), `test get relevant id #2 context`);
+
+		assert.equal(getRelevantId([`test get relevant id (#1 context)`, `#1`, ``]), `test get relevant id #1`);
+
+		assert.equal(getRelevantId([`test get relevant id (#3 context)`, `#3`, ``]), `test get relevant id`);
 	}
 });
